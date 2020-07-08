56b10cf6b8bbfdb6a64a93a12786e419bd6480ca
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-07-08 19:13:42 +0300

    def word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,54 @@
 def word_count(s):
     # Your code here
+    """Hello, my cat. And my cat doesn't say "hello" back."""
+    word_dict = {}
+    s = s.lower().split()
+    for word in s:
+        if word not in word_dict:
+            word_dict[word] = 1
+        word_dict[word] += 1
+    return word_dict
+
+   
+
+
+    """
+    for word in s:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] +=1
+    return word_dict
+print(word_count("Hello"))
+    """
+
+
+"""
+    counts = dict()
+    words = s.split()
+
+    for words in s:
+        if words.split():
+            continue
+        words = words.lower()
+
+        if words not in counts:
+            counts[words] = 1
+        else:
+            counts[words] += 1
+    return counts
+    """
+
+"""
+    for word in words:
+        if words in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
+# print(word_count("Hello"))
+    """
+
 
 
 
