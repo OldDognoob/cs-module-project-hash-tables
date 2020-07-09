638352c944e721d17a693f2ef6d9a625ed7d9862
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-07-09 17:58:24 +0300

    def no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,41 @@
 def no_dups(s):
     # Your code here
 
+    """
+    # create a non_dups list
+    non_dups = []
+    
+    for i in no_dups:
+        if i not in no_dups:
+            no_dups.append(i)
+    return no_dups
+    
+    # input a string
+    str_word = s.lower().isspace()
+    for s in str_word:
+        if s  == '':
+            # return an empty string
+            return ''
+    """
+    # create a dict
+    dict = {}
+    # input a string of letters
+    letters = s.split()
+    for letter in letters:
+        if letter not in dict:
+            dict[letter] = letter
+    return ' '.join(dict.keys())
+
+        
+        
+            
+
+            
+    
+        
+        
+
+
 
 
 if __name__ == "__main__":
